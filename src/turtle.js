515389597ca3dbaf60f8695a07174060c2df7808
Lindsay Fei <feilindsay@gmail.com>
2020-08-19 03:35:26 -0400

    bug fix: clone state

--- src/turtle.js
+++ src/turtle.js
@@ -11,26 +11,20 @@ class LineTurtle {
      * @param {LSystem} lsystem
      */
     static renderLines = (lsystem, scene, renderer, camera) => {
-        var state = [{x: -.5, y: -.5, z: .2, angle: Math.PI/3}]
+        var state = [{x: -.6, y: -.6, z: .2, angle: Math.PI/3}]
+        var geometry = new Geometry();
+        geometry.vertices.push(new Vector3(state[0].x, state[0].y, state[0].z))
 
         for (let i = 0; i < lsystem.string.length; i ++) {
             var c = lsystem.string.charAt(i);
-            var cur = state[state.length - 1];
             switch (c) {
                 case 'F':
-                    var geometry = new Geometry();
+                    var cur = state[state.length - 1];
                     var newx = cur.x + (lsystem.length*Math.cos(cur.angle));
                     var newy = cur.y + (lsystem.length*Math.sin(cur.angle));
                     geometry.vertices.push(
-                        new Vector3(cur.x, cur.y, cur.z),
                         new Vector3(newx, newy, cur.z),
                     );
-                    var line = new Line(geometry, new LineBasicMaterial({
-                        color: 0xC8D6AF
-                    }));
-                    scene.add(line);
-	                renderer.render( scene, camera );
-
                     state[state.length - 1] = {...state[state.length - 1], ...{x: newx, y: newy}};
                     break;
                 case '+':
@@ -40,10 +34,18 @@ class LineTurtle {
                     state[state.length - 1].angle -= lsystem.angle
                     break;
                 case '[':
-                    state.push(state[state.length - 1]);
+                    state.push({...state[state.length - 1]});
                     break;
                 case ']':
+                    var line = new Line(geometry, new LineBasicMaterial({
+                        color: 0xC8D6AF
+                    }));
                     state.pop();
+                    var cur = state[state.length - 1];
+                    scene.add(line);
+                    renderer.render( scene, camera );
+                    geometry = new Geometry();
+                    geometry.vertices.push(new Vector3(cur.x, cur.y, cur.z))
                     break;
                 default:
                     break;
